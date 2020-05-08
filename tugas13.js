a29cebd7065709d06d5a4d13b13c04340cc070b5
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 21:26:18 +0700

    tugas13js
    mengumpulkan tugas 13 js

---
+++ tugas13.js
@@ -0,0 +1,16 @@
+function tugas13() {
+
+    var buah = [ "pisang", "jeruk"];
+
+    console.log(buah);
+
+    console.log("=======================================");
+
+    buah.unshift("Apel","Mangga")
+
+    return buah
+    
+    
+}
+
+console.log(tugas13());
